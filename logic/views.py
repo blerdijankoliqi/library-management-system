7aa18dc70da3364ef38cbf0d893318696c78a266
blerdijan <blerdijankoliqi@gmail.com>
2022-10-26 15:25:22 +0200

    Created base html page

--- logic/views.py
+++ logic/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Student
 
 # Create your views here.
-def renderNames(request):
+def renderLoginPage(request):
     return render(request, 'login.html')
+
+def renderDashboardPage(request):
+    return render(request, 'base.html')
 
